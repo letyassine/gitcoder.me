e373fae71d1008a1f535b96be9dc8d63d7349374
yassinehaimouch <yassinehaimouch@proton.me>
2025-07-31 00:41:01 +0100

    UPDATE

--- app/page.tsx
+++ app/page.tsx
@@ -159,15 +159,15 @@ export default function Home() {
       <footer className="border-overlay ml-0 flex max-w-3xl items-center justify-between border-x border-b px-8 py-5 xl:ml-96">
         <ul className="flex gap-2 text-lg">
           <li>
-            <Link href="https://github.com/letyassine" target="_blank">
-              <BsLinkedin />
-            </Link>
-          </li>
-          <li>
             <Link
               href="https://www.linkedin.com/in/letyassine/"
               target="_blank"
             >
+              <BsLinkedin />
+            </Link>
+          </li>
+          <li>
+            <Link href="https://github.com/letyassine" target="_blank">
               <BsGithub />
             </Link>
           </li>
