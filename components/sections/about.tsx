c7681d88e8264072c915c621bd5ac36aabb84be7
Yassine <yassinehaimouch@proton.me>
2025-08-14 17:02:41 +0100

    UPDATE

--- components/sections/about.tsx
+++ components/sections/about.tsx
@@ -10,12 +10,17 @@ export default function About() {
         src="/hero.jpg"
         className="grayscale transition-all duration-300 group-hover:grayscale-0"
       />
-      <div className="space-y-6 px-8 py-5.5">
+      <div className="space-y-3 px-8 py-5.5">
         <p>
-          I'm Yassine — a software engineer especially focused on frontend
-          development, architecting React/Next.js solutions, and creating
-          exceptional user experiences. I'm also passionate about content
-          creation and public speaking in the tech community,
+          I am a software engineer especially focused on frontend development,
+          architecting React/Next.js solutions, and creating exceptional user
+          experiences.
+        </p>
+        <p>
+          My passion for frontend technologies continually drives me to advance
+          my skill set and adopt the latest industry best practices, I'm also
+          passionate about content creation and public speaking in the tech
+          community.
         </p>
         <p>Powered by a carefully curated tech stack.</p>
       </div>
