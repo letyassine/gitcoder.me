9bb1bcc2b7ceed3cd72dd5a6cd3f5e9b6aae36a4
letyassine <yassinehaimouch@proton.me>
2025-09-16 17:37:41 +0100

    UPDATE

--- components/ui/theme-toggle.tsx
+++ components/ui/theme-toggle.tsx
@@ -1,13 +1,22 @@
 "use client";
+
 import { useTheme } from "next-themes";
+import { MdOutlineDarkMode, MdOutlineLightMode } from "react-icons/md";
 
 const ThemeToggle = () => {
   let { resolvedTheme, setTheme } = useTheme();
   let otherTheme = resolvedTheme === "dark" ? "light" : "dark";
 
   return (
-    <button className="cursor-pointer" onClick={() => setTheme(otherTheme)}>
-      <div className="size-3 rounded-full bg-black dark:bg-white" />
+    <button
+      className="dark:hover:bg-dark-gary hover:bg-dark-gary/10 flex cursor-pointer items-center justify-center rounded-full p-2"
+      onClick={() => setTheme(otherTheme)}
+    >
+      <MdOutlineDarkMode size={18} className="dark:hidden dark:text-white" />
+      <MdOutlineLightMode
+        size={18}
+        className="hidden dark:block dark:text-white"
+      />
     </button>
   );
 };
