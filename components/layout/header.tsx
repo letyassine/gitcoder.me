876a3e2a06b60a1301b679b96c1b4354d3401ef7
letyassine <yassinehaimouch@proton.me>
2025-12-04 13:58:58 +0400

    UPDATE

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -5,6 +5,19 @@ import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 import ThemeToggle from "../ui/theme-toggle";
 import Button from "../ui/button";
+import { IoClose } from "react-icons/io5";
+import { TbMenu3 } from "react-icons/tb";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { useLockBodyScroll } from "@uidotdev/usehooks";
+
+const BodyScrollLock = () => {
+  useLockBodyScroll();
+  return null;
+};
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState<boolean>(false);
@@ -26,11 +39,93 @@ const Header = () => {
       )}
     >
       <div className="cursed bg-fd-background/40 absolute inset-0 -z-10 h-full w-full backdrop-blur-xs" />
-      <div className="flex items-center justify-center gap-2 dark:text-white">
+      <Disclosure as="div" className="w-full sm:hidden" defaultOpen={true}>
+        {({ close }) => (
+          <>
+            <div className="flex w-full items-center justify-between">
+              <div className="flex items-center justify-center gap-2 dark:text-white">
+                <Link
+                  href="/"
+                  className="font-bold text-black dark:text-white"
+                  onClick={() => close()}
+                >
+                  GITCODER
+                </Link>
+                <div className="flex items-center gap-1.5 rounded-full bg-gray-100 py-0.5 pr-2 pl-1.5 dark:bg-gray-200/20">
+                  <div className="relative">
+                    <div className="size-2 rounded-full bg-green-500" />
+                    <div className="absolute inset-0 size-2 animate-ping rounded-full bg-green-500/60 opacity-75" />
+                  </div>
+                  <span className="text-[12px] font-medium">Open to work</span>
+                </div>
+              </div>
+              <div className="flex items-center justify-center gap-2">
+                <ThemeToggle />
+                <DisclosureButton className="group flex w-full items-center justify-between">
+                  <TbMenu3
+                    size={24}
+                    className="block size-6 group-data-open:hidden dark:text-white"
+                  />
+                  <IoClose
+                    size={24}
+                    className="hidden size-6 group-data-open:block dark:text-white"
+                  />
+                </DisclosureButton>
+              </div>
+            </div>
+            <DisclosurePanel className="absolute top-18 right-0 h-[calc(100vh-72px)] w-full">
+              <BodyScrollLock />
+              <div className="dark:bg-charcoal-black/80 relative h-full w-full bg-white/80 backdrop-blur-xl">
+                <div className="cursed bg-fd-background/40 absolute inset-0 -z-10 h-full w-full backdrop-blur-xs" />
+                <ul className="relative flex h-screen flex-col items-center gap-8 pt-8 text-center text-2xl leading-[14px] font-semibold tracking-tighter dark:text-white">
+                  <li>
+                    <DisclosureButton
+                      as="a"
+                      href="/#about"
+                      className="block p-4 hover:bg-white/20"
+                    >
+                      About
+                    </DisclosureButton>
+                  </li>
+                  <li>
+                    <DisclosureButton
+                      as="a"
+                      href="/#projects"
+                      className="block p-4 hover:bg-white/20"
+                    >
+                      Projects
+                    </DisclosureButton>
+                  </li>
+                  <li>
+                    <DisclosureButton
+                      as="a"
+                      href="https://drive.proton.me/urls/1Y3S29VG1M#AD4ndecOGlgQ"
+                      target="_blank"
+                      className="block p-4 hover:bg-white/20"
+                    >
+                      Resume
+                    </DisclosureButton>
+                  </li>
+                  <li>
+                    <DisclosureButton
+                      as="a"
+                      href="/#contact"
+                      className="bg-charcoal dark:text-charcoal-black hover:bg-charcoal/90 block w-fit p-4 text-white duration-75 dark:bg-white dark:hover:bg-white/90"
+                    >
+                      Contact
+                    </DisclosureButton>
+                  </li>
+                </ul>
+              </div>
+            </DisclosurePanel>
+          </>
+        )}
+      </Disclosure>
+      <div className="hidden items-center justify-center gap-2 sm:flex dark:text-white">
         <Link href="/" className="font-bold text-black dark:text-white">
           GITCODER
         </Link>
-        <div className="hidden items-center gap-1.5 rounded-full bg-gray-100 py-0.5 pr-2 pl-1.5 sm:flex dark:bg-gray-200/20">
+        <div className="flex items-center gap-1.5 rounded-full bg-gray-100 py-0.5 pr-2 pl-1.5 dark:bg-gray-200/20">
           <div className="relative">
             <div className="size-2 rounded-full bg-green-500" />
             <div className="absolute inset-0 size-2 animate-ping rounded-full bg-green-500/60 opacity-75" />
@@ -38,8 +133,7 @@ const Header = () => {
           <span className="text-[12px] font-medium">Open to work</span>
         </div>
       </div>
-
-      <ul className="flex items-center justify-center gap-4 text-[13px] leading-[14px] font-semibold tracking-tighter dark:text-white">
+      <ul className="hidden items-center justify-center gap-4 text-[13px] leading-[14px] font-semibold tracking-tighter sm:flex dark:text-white">
         <li className="leading-0 lg:hidden">
           <ThemeToggle />
         </li>
