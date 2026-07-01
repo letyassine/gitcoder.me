900311e2fc0b6091ecc307cce95835a1b9ce3ee7
Yassine H. <yassinehaimouch@proton.me>
2026-07-01 12:48:02 +0400

    Update redirect URL in ResumePage component

--- app/resume/page.ts
+++ app/resume/page.ts
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function ResumePage() {
-  redirect("https://drive.proton.me/urls/1Y3S29VG1M#AD4ndecOGlgQ");
+  redirect("https://drive.proton.me/urls/AF7Z1R99DW#lJOkEz7H9MDN");
 }
