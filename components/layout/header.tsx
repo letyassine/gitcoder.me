9671e0fbe54238ea074528e7ce56be515201185d
Yassine H. <yassinehaimouch@proton.me>
2026-07-22 19:12:43 +0400

    Update Resume links in header component

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -105,7 +105,7 @@ const Header = () => {
                   <li>
                     <DisclosureButton
                       as="a"
-                      href="https://drive.proton.me/urls/1YSWQN4HBM#Ra11uAKe5TGZ"
+                      href="https://drive.proton.me/urls/X729B20N6W#POXwkSTeyRVY"
                       target="_blank"
                       className="block p-4 hover:bg-white/20"
                     >
@@ -155,7 +155,7 @@ const Header = () => {
         </li>
         <li>
           <Link
-            href="https://drive.proton.me/urls/1YSWQN4HBM#Ra11uAKe5TGZ"
+            href="https://drive.proton.me/urls/X729B20N6W#POXwkSTeyRVY"
             target="_blank"
             className="hover:underline"
           >
